e8998cae9e3a200c2a7ac1cb958058b9672e6a61
Bakhodur Nazriev <bakhodur.nazriev@gmail.com>
2024-07-02 01:25:59 +0500

    Implement crud for todos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,7 +9,7 @@ import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, TodoListComponent],
-  imports: [HttpClientModule, BrowserModule, AppRoutingModule],
+  imports: [HttpClientModule, BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -15,12 +15,70 @@ interface Todo {
 })
 export class TodoListComponent implements OnInit {
   todos: Todo[] = [];
+  filteredTodos: Todo[] = [];
+  searchTerm: string = '';
+  newTodoTitle: string = '';
+  editTodoId: number | null = null;
 
   constructor(private todoService: TodoService) {}
 
   ngOnInit(): void {
     this.todoService.getTodos().subscribe((data: Todo[]) => {
-      this.todos = data;
+      this.todos = data.sort((a, b) => b.id - a.id);
+      this.filteredTodos = [...this.todos];
     });
   }
+
+  deleteTodo(todoId: number): void {
+    this.todos = this.todos.filter((todo) => todo.id !== todoId);
+    this.filteredTodos = this.filteredTodos.filter(
+      (todo) => todo.id !== todoId
+    );
+  }
+
+  filterTodos(): void {
+    this.filteredTodos = this.todos.filter((todo) =>
+      todo.title.toLowerCase().includes(this.searchTerm.toLowerCase())
+    );
+  }
+
+  addTodo(): void {
+    if (this.newTodoTitle.trim()) {
+      const newTodo: Todo = {
+        userId: 1,
+        id: Date.now(),
+        title: this.newTodoTitle,
+        completed: false,
+      };
+      this.todos.unshift(newTodo);
+      this.todos.sort((a, b) => b.id - a.id);
+      this.filterTodos();
+      this.newTodoTitle = '';
+    }
+  }
+
+  startEdit(todo: Todo): void {
+    this.editTodoId = todo.id;
+    this.newTodoTitle = todo.title;
+  }
+
+  cancelEdit(): void {
+    this.editTodoId = null;
+    this.newTodoTitle = '';
+  }
+
+  updateTodo(): void {
+    if (this.newTodoTitle.trim() && this.editTodoId !== null) {
+      this.todos = this.todos.map((todo) => {
+        if (todo.id === this.editTodoId) {
+          return { ...todo, title: this.newTodoTitle };
+        }
+        return todo;
+      });
+      this.filterTodos();
+      this.editTodoId = null;
+      this.newTodoTitle = '';
+    }
+  }
+  
 }
